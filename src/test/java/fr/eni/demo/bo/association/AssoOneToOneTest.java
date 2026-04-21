b45330f8976f0b2bae375c58c66cba4458b83ba2
jbarlet2025 <barletjb.perso@gmail.com>
2026-04-21 14:37:28 +0200

    :sparkles:[FEATURE](Ticket-2): Ajout test

--- src/test/java/fr/eni/demo/bo/association/AssoOneToOneTest.java
+++ src/test/java/fr/eni/demo/bo/association/AssoOneToOneTest.java
@@ -21,7 +21,6 @@ public class AssoOneToOneTest {
     @Autowired
     private EntityManager entityManager;
 
-
     @Test
     void test_save(){
 
@@ -48,7 +47,42 @@ public class AssoOneToOneTest {
         Assertions.assertThat(employeDB.getId()).isGreaterThan(0);
         Assertions.assertThat(employeDB.getAdresse().getId()).isGreaterThan(0);
 
+    }
 
+    @Test
+    void test_delete(){
+
+        Adresse adresse = Adresse.builder()
+                .rue("RueTest")
+                .codePostal("44000")
+                .ville("NANTES")
+                .build();
+
+        Employe employe = Employe.builder()
+                .nom("TEST")
+                .prenom("test")
+                .email("test@gmail.com")
+                .immatriculation("TEST01")
+                .numDom("0123456789")
+                .numPortable("0623456789")
+                .adresse(adresse)
+                .build();
+
+        entityManager.persist(employe);
+        entityManager.flush();
+
+        log.info(employe.toString());
+
+        Integer idEmploye = employe.getId();
+        Integer idAdresse = employe.getAdresse().getId();
+
+        employeRepository.delete(employe);
+
+        Employe employeDB = entityManager.find(Employe.class, idEmploye);
+        Adresse adresseDB = entityManager.find(Adresse.class, idAdresse);
+
+        Assertions.assertThat(employeDB).isNull();
+        Assertions.assertThat(adresseDB).isNull();
     }
 
 
