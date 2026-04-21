f3c3c41c10bf889a36afa334dfa2d1e301dd819d
jbarlet2025 <barletjb.perso@gmail.com>
2026-04-21 16:06:51 +0200

    :white_check_mark:[TU](Ticket-7): Ajout TU AssoOnetoOneBi

---
+++ src/test/java/fr/eni/demo/bo/association/AssoOneToOneBiTest.java
@@ -0,0 +1,56 @@
+package fr.eni.demo.bo.association;
+
+import fr.eni.demo.bo.stagiaire.DonneePerso;
+import fr.eni.demo.bo.stagiaire.EtudiantENI;
+import fr.eni.demo.dal.DonneePersoRepository;
+import fr.eni.demo.dal.EtudiantENIRepository;
+import jakarta.persistence.EntityManager;
+import lombok.extern.slf4j.Slf4j;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.data.jpa.test.autoconfigure.DataJpaTest;
+
+@DataJpaTest
+@Slf4j
+class AssoOneToOneBiTest {
+
+    @Autowired
+    private EtudiantENIRepository etudiantEniRepository;
+
+    @Autowired
+    private DonneePersoRepository donneePersoRepository;
+
+    @Autowired
+    private EntityManager em;
+
+    @Test
+    void test_save() {
+        EtudiantENI etudiant = EtudiantENI.builder()
+                .email("etudiant@ecole.fr")
+                .immatriculation("IMMAT1")
+                .build();
+
+        DonneePerso donneePerso = DonneePerso.builder()
+                .nom("CACA")
+                .prenom("Jean-Baptiste")
+                .build();
+
+        etudiant.setDonneePerso(donneePerso);
+        donneePerso.setEtudiantENI(etudiant);
+
+        EtudiantENI etudiantDB = etudiantEniRepository.save(etudiant);
+
+        log.info(etudiantDB.toString());
+
+        Assertions.assertThat(etudiantDB.getDonneePerso().getId()).isGreaterThan(0);
+    }
+
+    @Test
+    void test_delete() {
+
+    }
+
+}
+
+
